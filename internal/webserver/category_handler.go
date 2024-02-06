f239909d2a57316ba1093a122d984db9e01cef74
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-02-06 15:40:04 -0300

    feat:Category web handler, getCategories

---
+++ internal/webserver/category_handler.go
@@ -0,0 +1,25 @@
+package webserver
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/hart42/ecomerce-go-fullcycle-/internal/service"
+)
+
+type WebCategoryHandler struct {
+	CategoryService *service.CategoryService
+}
+
+func NewWebCategoryHandler(categoryService *service.CategoryService) *WebCategoryHandler {
+	return &WebCategoryHandler{CategoryService: categoryService}
+}
+
+func (wch *WebCategoryHandler) GetCategories(w http.ResponseWriter, r *http.Request) {
+	categories, err := wch.CategoryService.GetCategories()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(categories)
+}
