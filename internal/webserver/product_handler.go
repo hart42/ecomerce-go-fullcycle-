77a9f6731e4135e86451999faa59f87145419190
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-02-06 16:10:33 -0300

    feat:Product web handler, getProduct by id

--- internal/webserver/product_handler.go
+++ internal/webserver/product_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/hart42/ecomerce-go-fullcycle-/internal/service"
 )
 
@@ -22,4 +23,18 @@ func (wph *WebProductHandler) GetProducts(w http.ResponseWriter, r *http.Request
 		return
 	}
 	json.NewEncoder(w).Encode(products)
+}
+
+func (wph *WebProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+	product, err := wph.ProductService.GetProduct(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(product)
 }
