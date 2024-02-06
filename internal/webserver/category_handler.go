b823459c4ee07fd35d4d15f3ebde3a2407bd46f9
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-02-06 15:48:46 -0300

    feat:Category web handler, getCategory by id

--- internal/webserver/category_handler.go
+++ internal/webserver/category_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/hart42/ecomerce-go-fullcycle-/internal/service"
 )
 
@@ -17,6 +18,20 @@ func NewWebCategoryHandler(categoryService *service.CategoryService) *WebCategor
 
 func (wch *WebCategoryHandler) GetCategories(w http.ResponseWriter, r *http.Request) {
 	categories, err := wch.CategoryService.GetCategories()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(categories)
+}
+
+func (wch *WebCategoryHandler) GetCategory(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+	categories, err := wch.CategoryService.CreateCategory(id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
