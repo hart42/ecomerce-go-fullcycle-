e2142ef6999e74a72b846c12530feaee1a4f0db4
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-02-06 16:08:25 -0300

    feat:Product web handler, getProducts

---
+++ internal/webserver/product_handler.go
@@ -0,0 +1,25 @@
+package webserver
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/hart42/ecomerce-go-fullcycle-/internal/service"
+)
+
+type WebProductHandler struct {
+	ProductService *service.ProductService
+}
+
+func NewWebProductHandler(productService *service.ProductService) *WebProductHandler {
+	return &WebProductHandler{ProductService: productService}
+}
+
+func (wph *WebProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := wph.ProductService.GetProducts()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(products)
+}
