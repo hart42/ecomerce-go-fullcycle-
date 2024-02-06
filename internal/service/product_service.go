725b2695ce8ac6166ebbba7a3976e023cdf8da71
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-02-06 15:26:52 -0300

    feat:Product Service CreateProduct

--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -20,3 +20,12 @@ func (ps *ProductService) GetProducts() ([]*entity.Product, error) {
 	}
 	return products, nil
 }
+
+func (ps *ProductService) CreateProduct(name, description, categoryID, imageURL string, price float64) (*entity.Product, error) {
+	product := entity.NewProduct(name, description, price, categoryID, imageURL)
+	_, err := ps.ProductDb.CreateProduct(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, err
+}
